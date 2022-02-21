c24e8b62c0d9a7b7aae8a5afe9fbed45c41954e3
victoria <vika.feduanina@gmail.com>
2022-02-21 09:33:42 +0300

    connect map

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react"
 import styles from './ProfilePage.module.scss';
 import { Button } from '../../UI/button/Button';
 import { thunkGetProfile } from '../../store/thunks';
 import { useAppDispatch } from '../../store/store';
+import GoogleMapReact from 'google-map-react';
+import { Marker } from "./Marker"
 
 export const ProfilePage: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -11,31 +13,71 @@ export const ProfilePage: React.FC = () => {
     dispatch(thunkGetProfile());
   }, []);
 
+const defaultPropsMap = {
+    center: {
+      lat: 53.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  const handleApiLoaded = (map:any) => {
+    console.log(map)
+  }
   return (
-    <>
-      ProfilePage
+    <div className={styles.containerProfilePage}>
       <div className={styles.containerFirstBlock}>
         <img src="" alt="" />
         <Button title="Редактировать профиль" onClick={() => ''} />
       </div>
+
       <div className={styles.containerSecondBlock}>
-        <p>name</p>
-        <div>
-          <div>profecional</div>
-          <div>dream</div>
-          <div>contacts</div>
+        <p>Ирина Чекунова Алексеевна</p>
+
+        <div className={styles.containerInformationBlocks}>
+          <div className={styles.infoBlock}>
+            <p>Возможности</p>
+            <p>list</p>
+          </div>
+          <div className={styles.infoBlock}>
+            <p>Потребности</p>
+            <p>list</p>
+          </div>
+          <div className={styles.infoBlock}>
+            <p>contacts</p>
+            <p>list</p>
+          </div>
         </div>
-        <p>gender</p>
+
+        <p>Пол: <span>Женский</span></p>
         <span>
-          <p>date 1</p> <p>date 2</p>
+          <p>Дата рождения: <span>12.12.2000</span></p>
+          <p>Дата смерти: <span>12.12.2000</span></p>
         </span>
         <span>
-          <p>father</p>
-          <p>mother</p>
+          <p>Отец: <span>Алексей</span></p>
+          <p>Мать: <span>Ирина</span></p>
         </span>
-        <p>location</p>
-        <div>map</div>
+        <p>Местоположение: <span>Россия, Москва, ул. Рыба 57</span></p>
+        <div>
+          <div style={{ height: '50vh', width: '100%' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: "AIzaSyC3u8u23ct68CEAxVm984B5h2lyFmJgH64&region=RU&language=ru" }}
+              defaultCenter={defaultPropsMap.center}
+              defaultZoom={defaultPropsMap.zoom}
+              yesIWantToUseGoogleMapApiInternals
+              onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map)}
+            >
+              <Marker
+                lat={53.955413}
+                lng={30.337844}
+              />
+            </GoogleMapReact>
+          </div>
+
+        </div>
       </div>
-    </>
+
+    </div>
   );
 };
